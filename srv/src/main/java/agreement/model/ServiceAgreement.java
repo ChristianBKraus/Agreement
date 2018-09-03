4ffd3ef59a3e717e9d5479b87fe2d0e35e1ae053
ChristianBKraus <Christian.b.kraus@Web.de>
2018-09-03 19:44:31 +0200

    added srv - java

---
+++ srv/src/main/java/agreement/model/ServiceAgreement.java
@@ -0,0 +1,5 @@
+package jupiterpa.serviceagreement.model;
+
+public class ServiceAgreement {
+
+}
---
+++ srv/src/main/java/agreement/service/ServiceInterface.java
@@ -0,0 +1,11 @@
+package jupiterpa.serviceagreement.service;
+
+import jupiterpa.serviceagreement.model.*;
+
+public interface ServiceInterface {
+	public void initialize();
+	public void validateServiceAgreement(ServiceAgreement agreement) throws ValidationException;
+	public void validateServiceReceiver(ServiceReceiver request) throws ValidationException;
+	public void validatePlannedCost(PlannedCost cost) throws ValidationException;
+	public void validate() throws ValidationException;
+}
